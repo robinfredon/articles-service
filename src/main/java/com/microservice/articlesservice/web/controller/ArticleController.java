2cf5664143ae9678f380934632dbb6f7b4e402d3
Robin FREDON <robin.fredon@gmail.com>
2021-02-04 16:59:25 +0100

    Add Logger

--- src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
+++ src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
@@ -6,7 +6,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.assertj.core.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
@@ -33,24 +34,30 @@ import com.microservice.articlesservice.web.exceptions.PrixArticleIncorrectExcep
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @Api(description = "API pour les opérations CRUD sur les articles")
 public class ArticleController {
-	
+
 	@Autowired
 	private ArticleDAO articleDAO;
+  private static final Logger logger = LoggerFactory.getLogger (ArticleController.class);
+  @Autowired
+  private HttpServletRequest requestContext ;
 
 	@RequestMapping(value = "/Articles", method = RequestMethod.GET)
 	public MappingJacksonValue listeArticles() {
-	 List<Article> articles = articleDAO.findAll();
-	 String filters[] = new String[] { "prixAchat", "marge" };
-	 SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept(filters);
-	 FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
-	 MappingJacksonValue articlesFiltres = new MappingJacksonValue(articles);
-	 articlesFiltres.setFilters(listDeNosFiltres);
-	 return articlesFiltres;
+	  logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
+	  List<Article> articles = articleDAO.findAll();
+	  String filters[] = new String[] { "prixAchat", "marge" };
+	  SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept(filters);
+	  FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
+	  MappingJacksonValue articlesFiltres = new MappingJacksonValue(articles);
+	  articlesFiltres.setFilters(listDeNosFiltres);
+	  return articlesFiltres;
 	}
-	
+
 	@GetMapping(value = "/AdminArticles")
 	public MappingJacksonValue listeArticlesAdmin() {
 		 List<Article> articles = articleDAO.findAll();
@@ -60,10 +67,11 @@ public class ArticleController {
 		 articlesFiltres.setFilters(listDeNosFiltres);
 		 return articlesFiltres;
 	}
-	
+
 	@ApiOperation(value = "Récupère un article grâce à son ID à	condition que celui-ci soit en stock!")
 	@RequestMapping(value = "/Articles/{id}", method = RequestMethod.GET)
 	public MappingJacksonValue afficherUnArticle(@PathVariable int id) {
+	   logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
 		 Article articles = articleDAO.findById(id);
 			if (articles == null)
 				throw new ArticleIntrouvableException("Article introuvable");
@@ -73,8 +81,9 @@ public class ArticleController {
 		 MappingJacksonValue articlesFiltres = new MappingJacksonValue(articles);
 		 articlesFiltres.setFilters(listDeNosFiltres);
 		 return articlesFiltres;
+
 	}
-	
+
 	@GetMapping(value = "/Articles/sort")
 	public MappingJacksonValue articlesSorted() {
 		 List<Article> articles = articleDAO.findAllByOrderByNomAsc();
@@ -84,7 +93,7 @@ public class ArticleController {
 		 articlesFiltres.setFilters(listDeNosFiltres);
 		 return articlesFiltres;
 	}
-	
+
 	@GetMapping(value = "/test/articles/{prixLimit}")
 	public MappingJacksonValue testDeRequetes(@PathVariable int prixLimit) {
 		 List<Article> articles = articleDAO.findByPrixGreaterThan(prixLimit);
@@ -94,7 +103,7 @@ public class ArticleController {
 		 articlesFiltres.setFilters(listDeNosFiltres);
 		 return articlesFiltres;
 	}
-	
+
 	@GetMapping(value = "/test/articles/like/{recherche}")
 	public MappingJacksonValue testeDeRequetes(@PathVariable String	recherche) {
 		 List<Article> articles = articleDAO.findByNomLike("%"+recherche+"%");
@@ -104,12 +113,12 @@ public class ArticleController {
 		 articlesFiltres.setFilters(listDeNosFiltres);
 		 return articlesFiltres;
 	}
-	
+
 	@DeleteMapping (value = "/Articles/{id}")
 	public void supprimerArticle(@PathVariable int id) {
 	 articleDAO.deleteById(id);
 	}
-	
+
 	@PutMapping (value = "/Articles")
 	public void updateArticle(@RequestBody Article article) {
 	 articleDAO.save(article);
@@ -126,5 +135,5 @@ public class ArticleController {
 						 .path("/{id}").buildAndExpand(articleAdded.getId()).toUri();
 		 return ResponseEntity.created(location).build();
 	 }
-	
+
 }
